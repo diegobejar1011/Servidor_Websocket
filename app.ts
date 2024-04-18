edaa1436a05448b27454e09aea654475380b2ba0
diegobejar1011 <diegobejar2004@gmail.com>
2024-04-17 22:53:49 -0600

    Ver conexion implementado

--- app.ts
+++ app.ts
@@ -28,6 +28,11 @@ httpServer.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
+io.use((socket, next)=> {
+    console.log(socket.handshake);
+    next();
+})
+
 io.on("connection", (socket: Socket) => {
   console.log("Cliente conectado");
 
