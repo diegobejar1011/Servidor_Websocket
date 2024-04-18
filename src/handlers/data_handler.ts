1e8a958977769fd394ad88307e417133aae03986
diegobejar1011 <diegobejar2004@gmail.com>
2024-04-17 23:48:14 -0600

    Evento para el fronted

--- src/handlers/data_handler.ts
+++ src/handlers/data_handler.ts
@@ -5,6 +5,7 @@ export function dataHandler(io: Server, socket: Socket) {
   const sendMessage = (payload: any) => {
     try {
       console.log(payload);
+      io.emit((Events.SEND_MESSAGE, payload));
     } catch (error: any) {
       throw new Error(error.message);
     }
