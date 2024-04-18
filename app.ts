88c776e2737091e297b5967aea92254725621ca1
diegobejar1011 <diegobejar2004@gmail.com>
2024-04-17 22:32:16 -0600

    Inicializacion del proyecto

---
+++ app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import { Server, Socket } from "socket.io";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+  },
+  pingInterval: 1000,
+  pingTimeout: 2000,
+});
+
+httpServer.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("Cliente conectado");
+});
